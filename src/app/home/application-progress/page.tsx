d42cb6dfbcb81dcc2e515dfef9cdf22b3c3abedb
shanker-dev <shanker.pwc@gmail.com>
2025-04-13 21:00:06 +0530

    Update page.tsx

--- src/app/home/application-progress/page.tsx
+++ src/app/home/application-progress/page.tsx
@@ -358,7 +358,7 @@ export default function ProgressPage() {
               whileTap={{ scale: 0.98 }}
               className="px-4 py-2 text-sm font-medium text-white bg-orange-600 rounded-md hover:bg-orange-700"
             >
-              Go to Faire's Site
+           <p>   Go to Faire's Site </p>
             </motion.button>
             <motion.button 
               whileHover={{ scale: 1.02 }}
